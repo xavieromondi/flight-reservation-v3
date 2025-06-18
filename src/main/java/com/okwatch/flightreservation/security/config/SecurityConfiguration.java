6af7b07dd7ae755b8a8b05de43978110d5021c01
xavier <omondix214@gmail.com>
2025-06-18 21:39:23 +0300

    security config

--- src/main/java/com/okwatch/flightreservation/security/config/SecurityConfiguration.java
+++ src/main/java/com/okwatch/flightreservation/security/config/SecurityConfiguration.java
@@ -15,56 +15,44 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
     @Autowired
-    UserDetailsServiceImpl userDetailsService;
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/admin/showAddFlight").hasAuthority("ADMIN")
-
-
-
-                                .requestMatchers("/showReg", "/", "/index.html", "/registerUser", "/login", "/login/*","/reservation/saveFlight").permitAll().anyRequest().permitAll()
-
-
-
-                ).httpBasic(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
-
-
-        return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        return daoAuthenticationProvider;
-    }
+    private UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/admin/showAddFlight").hasAuthority("ADMIN")
+                        .requestMatchers("/showReg", "/", "/index.html", "/registerUser", "/login", "/login/*", "/reservation/saveFlight")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
 }
